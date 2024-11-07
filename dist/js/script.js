0202f86242537ed1993badc4cf23a4c2ba711af9
Feri Nurjaman <95166484+lahferii@users.noreply.github.com>
2024-11-07 18:20:51 +0700

    add route

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,10 +2,10 @@ import { showProducts } from "./modules/product.mjs"
 import { showCategories } from "./modules/category.mjs"
 import { showSingleProduct } from "./modules/single.mjs"
 
-if(window.location.pathname == "/index.html"){
+if(window.location.pathname == "/" || window.location.pathname == "/index.html"){
   showCategories()
   showProducts()
 }
-else if(window.location.pathname == "/single.html"){
+else if(window.location.pathname == "/single" || window.location.pathname == "/single.html"){
   showSingleProduct()
 }
